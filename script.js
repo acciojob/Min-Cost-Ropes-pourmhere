4f3b9e07676e6ae9be5d9da844d07593b880ca41
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-07-17 17:05:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	 let cost = 0;
+  let heap = [...arr];
+  heap.sort((a, b) => a - b);
+  
+  while (heap.length > 1) {
+    let first = heap.shift();
+    let second = heap.shift();
+    cost += first + second;
+    heap.push(first + second);
+    heap.sort((a, b) => a - b);
+  }
+  
+  return cost;
   
 }
 
